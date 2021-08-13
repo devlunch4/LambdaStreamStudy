bbc77023f2046a9f75a5efbab535f9bf39ced759
devlunch4 <devlunch4@gmail.com>
2021-08-14 01:41:43 +0900

    create intStream2.

--- src/StreamEx.java
+++ src/StreamEx.java
@@ -1,3 +1,4 @@
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamEx {
@@ -7,11 +8,28 @@ public class StreamEx {
     }
 
     public static void main(String[] args) {
+        //iterate
         // Stream<Integer> evenStream = Stream.iterate(0, n -> n + 2);
         // evenStream.forEach(n -> System.out.println(n));
 
+        //generate
         // Stream<Double> randomStream = Stream.generate(Math::random);
         // System.out.println(randomStream);
         // randomStream.forEach(n -> System.out.println(n));
+
+        // skip
+        // IntStream intStream = IntStream. rangeClosed(1, 10);
+        // intStream.skip(3).limit(5).forEach(System.out::print);
+
+        // distinct
+        IntStream intStream = IntStream.of(1, 2, 2, 3, 3, 3, 4, 5, 5, 6);
+        intStream.distinct().forEach(System.out::println);
+
+        System.out.println();
+        //filter
+        IntStream intStream2 = IntStream.rangeClosed(1, 10);
+        intStream2.filter(i -> i % 2 == 0).forEach(System.out::println);
+
+
     }
 }
