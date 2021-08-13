0e4a9d408d556d799eab73bc3c1b9d996aae18e3
devlunch4 <devlunch4@gmail.com>
2021-08-13 19:09:52 +0900

    fixed error

--- src/LambdaEx.java
+++ src/LambdaEx.java
@@ -1,8 +1,10 @@
-public class Lambda {
+import java.util.Arrays;
+
+public class LambdaEx {
     //expression original
     public void exp_original() {
         int[] arr = new int[5];
-        Arrays.setAll(arr, (i) -> (int) Math.randon() * 5) + 1;
+        Arrays.setAll(arr, (i) -> (int) (Math.random() * 5) + 1);
     }
 
     //expresssion lambda method
